64be9533c0f8befc68c2944da9568c732ae007b7
PaulinaGtz <paulinagt16@gmail.com>
2022-06-16 22:46:11 -0500

    Últimos cambios

--- Cv.php
+++ Cv.php
@@ -51,7 +51,7 @@ $rowCursos = mysqli_fetch_array($queryCursos);
 // cursos
 $usuarioHab = "SELECT * FROM habilidades WHERE correo = '$correoUsuario'"; 
 $queryHab = mysqli_query($conn, $usuarioHab);
-$rowHab = mysqli_fetch_array($queryHab);
+$rowHab = mysqli_fetch_array($queryHab);*/
 
 ?>
 
@@ -70,9 +70,15 @@ $rowHab = mysqli_fetch_array($queryHab);
 
 <div>
   <br><br>
-  <h1 id="google_translate_element">CV</h1>
-  <h2 class="subtitulo"> Datos personales </h2>
-    <h4 class="texto">Fecha de Nacimiento: <?php echo $row['fechaNac']; ?> </h4>
+  <div id="header"></div>
+  <div class="left"></div>
+  <div class="stuff"> 
+  <h1 id="google_translate_element" class=""title >CV</h1>
+  <br><br>
+  <h2 class="head"> Nombre</h2><br>
+  <br>
+  <h2 class="head"> Datos personales </h2><br>
+    <h4>Fecha de Nacimiento: <?php echo $row['fechaNac']; ?> </h4>
     <h4>RFC: <?php echo $row['RFC']; ?> </h4>
     <h4>Ciudad: <?php echo $row['ciudad']; ?> </h4>
     <h4>Código Postal: <?php echo $row['codigo_postal']; ?> </h4>
@@ -80,29 +86,25 @@ $rowHab = mysqli_fetch_array($queryHab);
     <h4>Número de Calle: <?php echo $row['num_calle']; ?></h4>
 
   <br>
-  <h2> Situación Laboral
+  <h2 class="head"> Situación Laboral </h2> <br>
    <h4>Situación Laboral: <?php echo $row['sitLab']; ?> </h4>
-  </h2>
   <br>
-  <h2> Experiencia Laboral
+  <h2 class="head"> Experiencia Laboral</h2> <br>
     <h4>Empresa: <?php echo $rowInfoLab['empresa']; ?> </h4>
     <h4>Descripción: <?php echo $rowInfoLab['descripcion']; ?> </h4>
     <h4>Fecha Inicial: <?php echo $rowInfoLab['fechaInicial']; ?> </h4>
     <h4>Fecha Final: <?php echo $rowInfoLab['fechaFinal']; ?> </h4>
-  </h2>
   <br>
-  <h2> Formación acádemica
+  <h2 class="head"> Formación acádemica</h2> <br>
   <h4>Escuela: <?php echo $rowEdu['escuela']; ?> </h4>
   <h4> Carrera: <?php echo $rowEdu['carrera']; ?> </h4>
   <h4>Grado de educación: <?php echo $rowEdu['gradoEducacion']; ?> </h4>
   <h4>Fecha de graduación: <?php echo $rowEdu['fechaGrad']; ?></h4>
-  </h2>
   <br>
-  <h2> Descripción
+  <h2 class="head"> Descripción </h2> <br>
   <h4>Certificaciones: <?php echo $rowCert['certificacion']; ?> </h4>
   <h4>Cursos: <?php echo $rowCursos['curso']; ?> </h4>
   <h4>Habilidades: <?php echo $rowHab['experiencia_habil']; ?> </h4>
-  </h2>
   <br>
 </div>
 <script type="text/javascript">
